78ba7f17b5b0ffdf58168697f1c6e79555184a5d
rberto4 <92097805+rberto4@users.noreply.github.com>
2025-11-01 14:56:13 +0100

    Clean repository + add rob branch

--- android/app/src/main/kotlin/com/example/luma/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.luma
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/controller/interfaces/chat_interface.dart
+++
@@ -1,8 +0,0 @@
-import 'package:luma/models/chat/message.dart';
-
-abstract class ChatInterface {
-  Future<void> sendMessage(String chatId, Message message);
-  Stream<List<Message>> getMessagesStream(String chatId);
-  Future<void> deleteChatIfNotNearby(String currentUserId, String otherUserId, bool isNearby);
-  Future<bool> hasUnreadMessages(String chatId, String currentUserId);
-}
